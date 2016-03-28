85c34597b53fb49c8d28ca35d2f41bf0bcae5c71
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-03-28 23:24:10 +0300

    Added task 04 and 05

---
+++ CSharp/ExamPracticePartOne/01. SampleTest2012/04. BinaryDigitsCount/BinaryDigitsCount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.BinaryDigitsCount
+{  
+    class BinaryDigitsCount
+    {
+        static void Main(string[] args)
+        {
+            int b = int.Parse(Console.ReadLine());
+            uint n = uint.Parse(Console.ReadLine());
+           
+            List<uint> inputList = new List<uint>();
+            for (int i = 0; i < n; i++)
+            {
+                inputList.Add(uint.Parse(Console.ReadLine()));
+            }
+               
+            List<uint> results = new List<uint>();
+            foreach (uint num in inputList)
+            {
+                if(b == 0)
+                {                   
+                    results.Add(CountZeroes(num));
+                }
+                else
+                {
+                    results.Add(CountOnes(num));
+                }                   
+            }
+          
+            foreach (var num in results)
+            {
+                Console.WriteLine(num);
+            }                                  
+        }     
+                
+        static uint CountOnes(uint num)
+        {
+            uint count = 0;
+       
+            while (num > 0)
+            {
+                count = count + 1;
+                num = num & (num - 1);
+            }
+
+            return count;
+        }
+
+        static uint CountZeroes(uint num)
+        {
+            uint count = 0;
+            while(num != 0)
+            {
+                if((num & 1) != 1)
+                {
+                    count++;
+                }
+
+                num >>= 1;
+            }
+
+            return count;
+        }      
+    }
+}
---
+++ CSharp/ExamPracticePartOne/01. SampleTest2012/05. SubsetSums/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.SubsetSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sumToCheck = long.Parse(Console.ReadLine());
+            byte numberOfElements = byte.Parse(Console.ReadLine());
+            long[] inputNumbers = new long[numberOfElements];
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                inputNumbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int totalCombinations = (1 << numberOfElements) - 1;
+            long subsetsCounter = 0;
+            for (int i = 1; i < totalCombinations; i++)
+            {
+                List<long> subset = new List<long>();
+                for (int j = 0; j <= numberOfElements; j++)
+                {
+
+                    if ((((1 << j) & i) >> j) == 1)
+                    { 
+                        subset.Add(inputNumbers[j]);
+                    }
+                }
+
+                if (subset.Sum() == sumToCheck)
+                {
+                    subsetsCounter++;                  
+                }
+            }
+
+            if (subsetsCounter == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(subsetsCounter);
+            }
+        }
+    }
+}
