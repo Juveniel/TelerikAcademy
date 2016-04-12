80b62dd55c55c778b70ea4bff8e0be4d2405d46f
Dimitar <dimtar.kalaydzhievv@gmail.com>
2016-04-12 18:52:41 +0300

    Added task 4 Bat Goiko's Tower

---
+++ CSharp/ExamPracticePartOne/03. February2015Morning/03. SaddyKopper/SaddyKopper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SaddyKopper
+{
+    class SaddyKopper
+    {     
+        static int iterationsCount = 0;
+
+        static void Main(string[] args)
+        {       
+            long input = long.Parse(Console.ReadLine());
+            TransformNumber(input);
+        }
+
+        static long TransformNumber(long num)
+        {
+            List<long> productSums = new List<long>();
+
+            while (num.ToString().Length > 1)
+            {
+                num /= 10;
+                productSums.Add(FindEvenPositionsSum(long.Parse(num.ToString())));     
+            }
+            iterationsCount++;
+
+            long product = GetProductOfSums(productSums);            
+            int productDigits = GetDigitsCount(product);        
+
+            if (productDigits == 1 && iterationsCount < 10)
+            {
+                Console.WriteLine(iterationsCount);
+                Console.WriteLine(product);
+            }
+            else if (productDigits > 1 && iterationsCount < 10)
+            {
+                long newNum = product;
+                product = TransformNumber(newNum);              
+            }
+            else
+            {
+                Console.WriteLine(product);                
+            }
+
+            return product;
+        }
+
+        static long FindEvenPositionsSum(long input)
+        {
+            long sum = 0;
+            /*  char[] inputPositions = input.ToString().ToCharArray();
+
+               for (int i = 0; i < inputPositions.Length; i++)
+               {
+                   if(i % 2 == 0)
+                   {
+                       sum += int.Parse(inputPositions[i].ToString());
+                   }
+               }     */
+         
+
+            while(input != 0)
+            {
+                if(input % 2 == 0)
+                {
+                    sum += input;
+                }
+
+                input /= 10;
+            }
+
+            return sum;
+        }
+
+        static long GetProductOfSums(List<long> numbers)
+        {
+            long prod = 1;
+            foreach (int value in numbers)
+            {
+                prod *= value;
+            }
+
+            return prod;
+        }
+
+        static int GetDigitsCount(long num)
+        {
+            int length = num.ToString().Length;
+
+            return length;
+        }
+    }
+}
---
+++ CSharp/ExamPracticePartOne/06. June2013Exam2/04. BatGoikosTower/BatGoikosTower.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04.BatGoikosTower
+{
+    class BatGoikosTower
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char dot = '.';
+            char forwardSlash = '/';
+            char backslash = '\\';
+            char whSpace = ' ';
+
+            int dotCount = n - 1;
+            int whSpaceCount = 0;
+            int dashRowIndex = 1;
+            int beamCounter = 2; 
+
+            for (int i = 0; i < n; i++)
+            {
+                if(i == dashRowIndex)
+                {
+                    dashRowIndex = (i + beamCounter);
+                    beamCounter++;
+                    whSpace = '-';
+                }
+                else if(i == 0)
+                {
+                    whSpace = ' ';
+                }
+                else
+                {
+                    whSpace = '.';
+                }
+
+                Console.WriteLine(new string(dot, dotCount) + forwardSlash + new string(whSpace, whSpaceCount   ) +
+                                  backslash +  new string(dot, dotCount));
+
+                dotCount--;
+                whSpaceCount += 2;                           
+            }
+        }
+    }
+}
