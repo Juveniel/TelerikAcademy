7343f5b4757991921924eab453fb4f6d76c872a3
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-04-13 00:57:02 +0300

    Added task corrections

---
+++ CSharp/ExamPracticePartOne/02. TestExam2012/03. Trapezoid/Trapezoid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Trapezoid
+{
+    class Trapezoid
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var widthTop = n;
+            var widthBottom = n * 2;
+            var height = n + 1;
+
+            char symbol = '*';
+            char dot = '.';
+
+            Console.WriteLine(new string(dot, n) + new string(symbol, n));
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine(new string(dot, n - i) + symbol + new string(dot, n - 2 + i) + symbol);
+            }
+            Console.WriteLine(new string(symbol, widthBottom));
+        }
+    }
+}
---
+++ CSharp/ExamPracticePartOne/03. February2015Morning/03. SaddyKooper/SaddyKooper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace _03.SaddyKopper
+{
+    class SaddyKopper
+    {
+        static int iterationsCount = 0;
+
+        static void Main(string[] args)
+        {
+            long input = long.Parse(Console.ReadLine());
+            TransformNumber(input);
+        }
+
+        static BigInteger TransformNumber(BigInteger num)
+        {
+            List<long> productSums = new List<long>();
+
+            while (num.ToString().Length > 1)
+            {
+                num /= 10;
+                productSums.Add(FindEvenPositionsSum(long.Parse(num.ToString())));
+            }
+            iterationsCount++;
+
+            BigInteger product = GetProductOfSums(productSums);
+            var productDigits = GetDigitsCount(product);
+
+            if (productDigits == 1 && iterationsCount < 10)
+            {
+                Console.WriteLine(iterationsCount);
+                Console.WriteLine(product);
+            }
+            else if (productDigits > 1 && iterationsCount < 10)
+            {
+                BigInteger newNum = product;
+                product = TransformNumber(newNum);
+            }
+            else
+            {
+                Console.WriteLine(product);
+            }
+
+            return product;
+        }
+
+        static long FindEvenPositionsSum(long input)
+        {
+            int counter = 0;
+            long sum = 0;
+            long x = 0;
+
+            long[] digitsArr = new long[input.ToString().Length];
+            while (input > 0)
+            {
+
+                x = input % 10;
+                digitsArr[counter] = x;
+
+                input = input / 10;
+                counter++;
+            }
+
+            Array.Reverse(digitsArr);
+            for(int i = 0; i < digitsArr.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    sum += digitsArr[i];
+                }
+            }
+
+            return sum;
+        }
+
+        static long GetProductOfSums(List<long> numbers)
+        {
+            long prod = 1;
+            foreach (int value in numbers)
+            {
+                prod *= value;
+            }
+
+            return prod;
+        }
+
+        static int GetDigitsCount(BigInteger num)
+        {
+            return num.ToString().Length;
+        }
+    }
+}
