3eca5ddde3ebbd853dc5b7232f4b0b501dcbc05c
Dimitar <dimtar.kalaydzhievv@gmail.com>
2016-04-13 18:03:42 +0300

    Removed empty project from solution

---
+++ CSharp/ExamPracticePartOne/07. December2013Exam1/01. TwoFourEight/TwoFourEight.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.TwoFourEight
+{
+    class TwoFourEight
+    {
+        static void Main(string[] args)
+        {
+            List<long> numebrsList = new List<long>();
+            for(int i = 0; i < 3; i++)
+            {
+                long n = long.Parse(Console.ReadLine());
+                numebrsList.Add(n);          
+            }
+
+            long r = TransformCode(numebrsList);
+            
+            if(r % 4 == 0)
+            {
+                Console.WriteLine(r / 4);
+            }
+            else
+            {
+                Console.WriteLine(r % 4);
+            }
+
+            Console.WriteLine(r);
+
+        }  
+        
+        static long TransformCode(List<long> numbers)
+        {
+            long transformedCode = 0;
+            long num = numbers[1];
+
+            switch (num)
+            {
+                case 2:
+                    transformedCode = numbers[0] % numbers[2];
+                    break;
+                case 4:
+                    transformedCode = numbers[0] + numbers[2];
+                    break;
+                case 8:
+                    transformedCode = numbers[0] * numbers[2];
+                    break;
+                default:
+                    break;
+            }
+            
+            return transformedCode;
+        } 
+    }
+}
---
+++ CSharp/ExamPracticePartOne/07. December2013Exam1/02. NightmareOnCodeStreet/NightmareOnCodeStreet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.NightmareOnCodeStreet
+{
+    class NightmareOnCodeStreet
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] digits = input.ToCharArray();
+
+            long sum = 0;
+            int oddDigitsCount = 0;
+            for(int i = 0; i < digits.Length; i++)
+            {
+                if(i % 2 == 1 && Char.IsDigit(digits[i]))
+                {             
+                    sum += long.Parse(digits[i].ToString());
+                    oddDigitsCount++;
+                }
+            }
+
+            Console.WriteLine("{0} {1}", oddDigitsCount, sum);
+        }
+    }
+}
