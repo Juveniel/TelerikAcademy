b4dd4e7b0237ada3ff317e40f9a017abe8cb6109
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-04-16 00:12:08 +0300

    Added tasks 1, 3 and 4

---
+++ CSharp/ExamPracticePartOne/10. February2015EveningExam1/01. Money/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.Money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+
+            int totalSheets = n * s;
+            double realms = (double)totalSheets / 400;
+            double totalPrice = realms * p;
+
+            Console.WriteLine("{0:F3}", totalPrice);
+
+        }
+    }
+}
---
+++ CSharp/ExamPracticePartOne/10. February2015EveningExam1/02. EncodingSum/EncodingSum.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.EncodingSum
+{
+    class EncodingSum
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+            char[] inputArr = input.ToCharArray();
+
+            long result = 0;
+            var symbol = '\u0040';
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                char current = inputArr[i];
+
+                if (IsInteger(current))
+                {
+                    result *= int.Parse(current.ToString());
+                }
+                else if (IsLetter(current))
+                {
+                    result += GetLetterIndex(current);
+                }
+                else if (current == symbol)
+                {
+                    break;
+                }
+                else
+                {
+                    result %= m;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static bool IsInteger(char c)
+        {
+            return Char.IsNumber(c);
+        }
+
+        static bool IsLetter(char c)
+        {
+            return Char.IsLetter(c);
+        }
+
+        static int GetLetterIndex(char c)
+        {
+            int index = char.ToUpper(c) - 64;
+
+            return index - 1;
+        }
+    }
+}
