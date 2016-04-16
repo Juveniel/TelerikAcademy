086726ffe8bbaddd5a0b340b19453cb9a61ef75c
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-04-16 11:04:43 +0300

    Added task 4

---
+++ CSharp/ExamPracticePartOne/10. February2015EveningExam1/04. Maslan/Maslan.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Numerics;
+
+namespace _04.Maslan
+{
+    class Maslan
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            int transformationsCount = 0;
+            bool finished = false;
+
+            while (n.Length > 1)
+            {
+                BigInteger numTransformed = 1;
+                int sum = 0;
+
+                char[] digits = n.ToCharArray();
+                for (int i = 0; i < digits.Length; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        sum += int.Parse(digits[i].ToString());
+                    }
+
+                    if (sum > 0)
+                    {
+                        numTransformed *= sum;
+                    }
+                }
+
+                if (sum != 0)
+                {
+                    numTransformed /= sum;
+                }
+
+                transformationsCount++;
+
+                n = numTransformed.ToString();
+
+                if (transformationsCount == 10)
+                {
+                    finished = true;
+                    break;
+                }
+            }
+
+            if (finished)
+            {
+                Console.WriteLine(n);
+            }
+            else
+            {
+                Console.WriteLine(transformationsCount);
+                Console.WriteLine(n);
+            }  
+        }   
+    }
+}
