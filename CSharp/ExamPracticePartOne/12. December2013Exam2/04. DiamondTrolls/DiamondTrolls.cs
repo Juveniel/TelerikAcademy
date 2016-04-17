e6d6d5a1c247d386c904d7c62bfa535b3ed2b7f1
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-04-17 12:54:52 +0300

    Added tasks 1,2, 4

---
+++ CSharp/ExamPracticePartOne/12. December2013Exam2/04. DiamondTrolls/DiamondTrolls.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.DiamondTrolls
+{
+    class DiamondTrolls
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int width = n * 2 + 1;
+            int height = (6 + ((n -3) / 2) * 3);
+
+            int innerHeight = height - n - 2;
+            int innerDots = ((width - innerHeight * 2) - 3) / 2;
+ 
+            Console.WriteLine(" " + new string('.', innerHeight + 1) + new string('*', width - 2 * innerHeight - 2) + new string('.', innerHeight + 1));
+            for(int i = 0; i < height - n - 2; i++)
+            {
+                Console.WriteLine(" " + new string('.', innerHeight) + "*" + new string('.', innerDots) + "*" + new string('.', innerDots) + "*" + new string('.', innerHeight));
+
+                innerHeight--;
+                innerDots++;
+            }
+            Console.WriteLine(" " + new string('*', width));
+
+            int bottomDots = innerDots - 1;
+            int bottomDotsOuter = 1;
+            for(int j = 0; j < n - 1; j++)
+            {
+                Console.WriteLine(" " + new string('.', bottomDotsOuter) + "*" + new string('.', bottomDots) + "*" + new string('.', bottomDots) + "*" + new string('.', bottomDotsOuter));
+         
+                bottomDots--;                  
+                bottomDotsOuter++;
+            }
+            Console.WriteLine(" " + new string('.',width - n - 1) + "*" + new string('.', width - n - 1));
+        }
+    }
+}
