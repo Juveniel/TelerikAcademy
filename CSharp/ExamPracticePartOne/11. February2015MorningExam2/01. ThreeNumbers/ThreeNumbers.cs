a75fc523d917a571bd7963a171450d4b65a32a16
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-04-17 11:36:00 +0300

    Added task 1,2,3

---
+++ CSharp/ExamPracticePartOne/11. February2015MorningExam2/01. ThreeNumbers/ThreeNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ThreeNumbers
+{
+    class ThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = new List<int>();
+            numbersList.Add(int.Parse(Console.ReadLine()));
+            numbersList.Add(int.Parse(Console.ReadLine()));
+            numbersList.Add(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(numbersList.Max());
+            Console.WriteLine(numbersList.Min());
+            Console.WriteLine("{0:F2}", numbersList.Average());
+        }
+    }
+}
---
+++ CSharp/ExamPracticePartOne/11. February2015MorningExam2/03. ConsoleApplication1/ConsoleApplication1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Numerics;
+
+namespace _03.ConsoleApplication1
+{
+    class ConsoleApplication1
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+
+            BigInteger productFirsTen = 1;
+            BigInteger productSecondTen = 1;
+            BigInteger current;
+
+            int lineCounter = 0;
+            while (line != "END")
+            {
+                if(lineCounter % 2 != 0)
+                {
+                    current = BigInteger.Parse(line);
+                    while (current > 0)
+                    {
+                        if (current % 10 > 0 && lineCounter <= 9)
+                        {
+                            productFirsTen *= current % 10;
+                        }
+                        else if(current % 10 > 0 && lineCounter > 9)
+                        {
+                            productSecondTen *= current % 10;
+                        }
+                        current /= 10;
+                    }
+                }
+
+                lineCounter++;
+
+                line = Console.ReadLine();
+            }
+
+            if(lineCounter <= 10)
+            {
+                Console.WriteLine(productFirsTen);
+            }
+            else
+            {
+                Console.WriteLine(productFirsTen);
+                Console.WriteLine(productSecondTen);
+            }
+        }      
+    }
+}
