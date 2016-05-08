e718d9d9f38b910c80aad710017af025555e8b0a
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-05-08 11:27:23 +0300

    Added tasks 7 and 8

---
+++ CSharp/10. NumeralSystems/07. OneSystemToOther/OneSystemToOther.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace _07.OneSystemToOther
+{
+    /// <summary>
+    /// Write a program to convert the number N from any numeral system of 
+    /// given base s to any other numeral system of base d.
+    /// </summary>
+    class OneSystemToOther
+    {
+        static void Main(string[] args)
+        {
+            string numToConvert = Console.ReadLine();
+            uint fromBase = uint.Parse(Console.ReadLine());
+            uint to = uint.Parse(Console.ReadLine());
+
+            string convertedNum = Convert(numToConvert, fromBase, to);
+            Console.WriteLine(convertedNum);
+        }
+
+        private static string Convert(string numberToConvert, uint from, uint to)
+        {
+            uint number = NToDecimal(numberToConvert, from);
+
+            if (number == 0)
+                return number.ToString();
+
+            return DecimalToN(to, number);
+        }
+
+        private static string DecimalToN(uint to, uint number)
+        {
+            StringBuilder result = new StringBuilder();
+            uint remainder;
+            char currentLetter;
+            while (number > 0)
+            {
+                remainder = number % to;
+
+                if (remainder < 10)
+                    currentLetter = (char)(remainder + '0');
+                else
+                    currentLetter = (char)(remainder + 55);
+
+                result.Insert(0, currentLetter);
+
+                number /= to;
+            }
+            return result.ToString();
+        }
+
+        private static uint NToDecimal(string numberToConvert, uint from)
+        {
+            uint number = 0;
+            byte currentValue;
+            numberToConvert = numberToConvert.ToUpper();
+
+            for (int current = 0; current < numberToConvert.Length; current++)
+            {
+                number *= from;
+                if (Char.IsLetter(numberToConvert[current]))
+                    currentValue = (byte)(numberToConvert[current] - 55);
+                else
+                    currentValue = (byte)(numberToConvert[current] - '0');
+
+                number += currentValue;
+            }
+            return number;
+        }
+    }
+}
---
+++ CSharp/10. NumeralSystems/08.  ShortToBinary/ShortToBinary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace _08.ShortToBinary
+{
+    /// <summary>
+    /// Write a program that shows the binary representation of given 16-bit signed integer number N
+    /// </summary>
+    class ShortToBinary
+    {
+        static void Main(string[] args)
+        {
+            short numToConvert = short.Parse(Console.ReadLine());
+
+            Console.WriteLine(ShortToBinary(numToConvert));
+        }
+
+        private static string ShortToBinary(short number)
+        {
+            if (number == 0)
+                return new String('0', 16);
+
+            StringBuilder binary = new StringBuilder();
+
+            for (int bit = 0; bit < 16; bit++)
+            {
+                binary.Insert(0, (number >> bit) & 1);
+            }
+
+            return binary.ToString();
+        }
+    }
+}
