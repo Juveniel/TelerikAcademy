c703cb4c888b428724ce516865e61e8ff8c9ef7d
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-05-11 00:56:40 +0300

    Added tasks 19 and 20

---
+++ CSharp/12. StringsAndTextProcessing/19. DatesFromTextInCanada/DatesFromTextInCanada.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace _19.DatesFromTextInCanada
+{
+    /// <summary>
+    /// Write a program that extracts from a given text all dates that match the format DD.MM.YYYY.
+    /// Display them in the standard date format for Canada.
+    /// </summary>
+    class DatesFromTextInCanada
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            var regex = new Regex(@"\b\d{2}\.\d{2}.\d{4}\b");
+            foreach (Match m in regex.Matches(text))
+            {
+                DateTime dt;
+                if (DateTime.TryParseExact(m.Value, "dd.MM.yyyy", null, DateTimeStyles.None, out dt))
+                {
+                    Console.WriteLine(dt.ToString(CultureInfo.GetCultureInfo("en-CA").DateTimeFormat));
+                }
+                    
+            }
+        }
+    }
+}
---
+++ CSharp/12. StringsAndTextProcessing/20. Palindromes/Palindromes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _20.Palindromes
+{
+    /// <summary>
+    /// Write a program that extracts from a given text all palindromes, e.g. ABBA, lamal, exe.
+    /// </summary>
+    class Palindromes
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            MatchCollection words = Regex.Matches(text, @"\b\w+\b");
+
+            foreach (var word in words)
+            {
+                if (IsPalindrome(word.ToString()))
+                {
+                    Console.WriteLine(word);
+                }
+            }              
+            Console.WriteLine();
+        }
+
+        private static bool IsPalindrome(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+
+                // Scan forward for a while invalid.
+                while (!char.IsLetterOrDigit(a))
+                {
+                    min++;
+                    if (min > max)
+                    {
+                        return true;
+                    }
+                    a = value[min];
+                }
+
+                // Scan backward for b while invalid.
+                while (!char.IsLetterOrDigit(b))
+                {
+                    max--;
+                    if (min > max)
+                    {
+                        return true;
+                    }
+                    b = value[max];
+                }
+
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+    }
+}
