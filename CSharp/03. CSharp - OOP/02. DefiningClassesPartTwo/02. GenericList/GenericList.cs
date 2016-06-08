e050406b541a6a01b13bc103203d3a39fa192020
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-06-09 02:02:20 +0300

    Added new methods to generic class

---
+++ CSharp/03. CSharp - OOP/02. DefiningClassesPartTwo/02. GenericList/GenericList.cs
@@ -0,0 +1,146 @@
+﻿namespace _02.GenericList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GenericList<T> where T : IComparable
+    {
+        private const int DefaultCapacity = 1000;
+        private T[] elements;
+
+        public GenericList(int capacity = DefaultCapacity)
+        {
+            this.Count = 0;
+            this.Capacity = capacity;
+            this.elements = new T[DefaultCapacity];
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count { get; set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.elements[index];
+            }
+
+            set
+            {
+                this.elements[index] = value;
+            }
+        }
+
+        public void Add(T element)
+        {
+            this.Count++;
+            this.Resize(this.Count);
+            this.elements[this.Count - 1] = element;
+        }
+
+        public int GetByIndex(T element)
+        {
+            return Array.IndexOf(this.elements, element);
+        }
+
+        public void RemoveAtIndex(int position)
+        {
+            if (position < 0 || position >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException("Position is outside of the bounds of the collection!");
+            }
+            else
+            {                
+                this.Count--;
+                this.Resize(this.Count);
+
+                for (int a = position; a < this.elements.Length - 1; a++)
+                {
+                    this.elements[a] = this.elements[a + 1];
+                }
+            }
+        }
+
+        public void InsertAtPosition(int position, T element)
+        {
+            if (position < 0 || position > this.Count)
+            {
+                throw new ArgumentOutOfRangeException("Position is outside of the bounds of the collection!");
+            }
+            else
+            {
+                this.Count++;
+                this.Resize(this.Count);
+
+
+
+                // store
+                //elements[position] = element; // add the new element
+
+                // shift the elements after the new elements to the right
+                T[] demo = new T[this.elements.Length];
+                for (int i = 0; i < this.elements.Length; i++)
+                {
+                    demo[(i ) % demo.Length] = this.elements[i];
+                }
+                demo[position] = element;
+
+                this.elements = demo;
+
+                // put the element we stored back
+                //elements[position + 1] = store;
+
+               /* for (int i = position + 2; i < this.Capacity; i++)
+                {
+                    elements[i] = elements[i - 1];
+                }
+                */
+               // this.elements[position] = element;
+            }
+        }
+
+        public void Clear()
+        {
+            this.Count = 0;
+            this.Capacity = DefaultCapacity;
+
+            this.elements = new T[this.Capacity];
+        }
+
+        public override string ToString()
+        {
+            if (this.Count == 0)
+            {
+                return "Empty list!";
+            }
+                
+            StringBuilder result = new StringBuilder();
+            result.Append("Element(s): ");
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                result.AppendFormat("{0}", this.elements[i].ToString());
+
+                if (i + 1 < this.Count)
+                {
+                    result.Append(",");
+                }                   
+            }
+
+            return result.ToString();
+        }
+
+        private void Resize(int capacity)
+        {
+            if (capacity > this.Capacity)
+            {
+                this.Capacity *= 2;
+                Array.Resize(ref this.elements, this.Capacity);
+            }
+        }
+    }
+}
---
+++ CSharp/03. CSharp - OOP/02. DefiningClassesPartTwo/02. GenericList/Test.cs
@@ -0,0 +1,48 @@
+﻿namespace _02.GenericList
+{
+    using System;
+
+    public class Test
+    {
+        public static void Main()
+        {
+            GenericList<int> elements = new GenericList<int>();
+
+            // Empty list
+            Console.WriteLine("\n" + elements);
+            Console.WriteLine("Count: {0}", elements.Count);
+            Console.WriteLine("Capacity: {0}", elements.Capacity);
+
+            // Auto-grow functionality
+            elements = new GenericList<int>(3);
+            elements.Add(1);
+            elements.Add(2);
+            elements.Add(3);
+            elements.Add(4);
+            elements.Add(5);
+            elements.Add(6);
+            elements.Add(7);
+
+            Console.WriteLine("\n" + elements);
+            Console.WriteLine("Count: {0}", elements.Count);
+            Console.WriteLine("Capacity: {0}", elements.Capacity);
+
+            // Insert, RemoveAt   
+            elements.InsertAtPosition(3, 4323);
+           // elements.InsertAtPosition(0, 3);
+          //  elements.InsertAtPosition(0, 2);
+           // elements.InsertAtPosition(0, 1);
+
+            //elements.RemoveAtIndex(0);
+           // elements.RemoveAtIndex(elements.Count - 1);
+
+            Console.WriteLine("\n" + elements);
+            Console.WriteLine("Count: {0}", elements.Count);
+            Console.WriteLine("Capacity: {0}", elements.Capacity);
+
+            // Contains, IndexOf
+            Console.WriteLine("\nContain element 2: {0}", elements.GetByIndex(2));
+            Console.WriteLine("Index of element 3: {0}", elements.GetByIndex(3));
+        }
+    }
+}
