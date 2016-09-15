6721d3a52f978c52d9f5250cf6ca11e8c3d8f4d0
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-09-15 23:27:12 +0300

    Added solution for Defensive programming topic homework

---
+++ CSharp/06. CSharp - High Quality Code Part Two/01. DefensiveProgramming/Assertions/Assertions.cs
@@ -0,0 +1,102 @@
+﻿namespace Assertions
+{
+    using System;
+    using System.Diagnostics;
+
+    internal class Assertions
+    {
+        internal static void Main()
+        {
+            var arr = new int[] { 3, -1, 15, 4, 17, 2, 33, 0 };
+            Console.WriteLine($"arr = [{string.Join(", ", arr)}]");
+            SelectionSort(arr);
+            Console.WriteLine($"sorted = [{string.Join(", ", arr)}]");
+
+            SelectionSort(new int[0]);
+            SelectionSort(new int[1]);
+
+            Console.WriteLine(BinarySearch(arr, -1000));
+            Console.WriteLine(BinarySearch(arr, 0));
+            Console.WriteLine(BinarySearch(arr, 17));
+            Console.WriteLine(BinarySearch(arr, 10));
+            Console.WriteLine(BinarySearch(arr, 1000));
+        }
+
+        public static void SelectionSort<T>(T[] arr)
+            where T : IComparable<T>
+        {
+            Debug.Assert(arr != null, "The array is null!");
+
+            for (var index = 0; index < arr.Length - 1; index++)
+            {
+                var minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
+                Swap(ref arr[index], ref arr[minElementIndex]);
+            }
+        }
+
+        private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex)
+            where T : IComparable<T>
+        {
+            Debug.Assert(arr != null, "The array is null!");
+            Debug.Assert(startIndex >= 0, "Start index is negative!");
+            Debug.Assert(endIndex >= 0, "End index is negative!");
+            Debug.Assert(startIndex <= endIndex, "Starting index is larger than the end index.");
+
+            var minElementIndex = startIndex;
+            for (var i = startIndex + 1; i <= endIndex; i++)
+            {
+                if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+                {
+                    minElementIndex = i;
+                }
+            }
+            return minElementIndex;
+        }
+
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            Debug.Assert(x != null, "X ix null.");
+            Debug.Assert(y != null, "Y is null.");
+
+            var oldX = x;
+            x = y;
+            y = oldX;
+        }
+
+        public static int BinarySearch<T>(T[] arr, T value) where T : IComparable<T>
+        {
+            Debug.Assert(arr != null, "Arr ix null.");
+            Debug.Assert(value != null, "Value is null.");
+
+            return BinarySearch(arr, value, 0, arr.Length - 1);
+        }
+
+        private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex)
+            where T : IComparable<T>
+        {
+            Debug.Assert(arr != null, "The array is null!");
+            Debug.Assert(startIndex >= 0, "Start index is negative!");
+            Debug.Assert(endIndex >= 0, "End index is negative!");
+            Debug.Assert(startIndex <= endIndex, "Starting index is larger than the end index.");
+
+            while (startIndex <= endIndex)
+            {
+                var midIndex = (startIndex + endIndex) / 2;
+                if (arr[midIndex].Equals(value))
+                {
+                    return midIndex;
+                }
+                if (arr[midIndex].CompareTo(value) < 0)
+                {
+                    startIndex = midIndex + 1;
+                }
+                else
+                {
+                    endIndex = midIndex - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ CSharp/06. CSharp - High Quality Code Part Two/01. DefensiveProgramming/Exceptions/Models/Student.cs
@@ -0,0 +1,105 @@
+﻿namespace Exceptions.Models
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+
+        public Student(string firstName, string lastName, IList<Exam> exams = null)
+        {           
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Exams = exams;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;                
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Firstname cannot be empty!");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;                
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Lastname cannot be empty!");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public IList<Exam> Exams { get; private set; }
+        
+        public IList<ExamResult> CheckExams()
+        {
+            if (this.Exams == null)
+            {
+                throw new ArgumentNullException($"Student: {this.GetFullName()}'s exam list is empty!");
+            }
+
+            if (this.Exams.Count == 0)
+            {
+                throw new ArgumentException($"Student: {this.GetFullName()} doesn't have any exams!");
+            }
+
+            var results = this.Exams.Select(e => e.Check()).ToList();
+
+            return results;
+        }
+
+        public double CalcAverageExamResultInPercents()
+        {
+            if (this.Exams == null)
+            {               
+                throw new ArgumentNullException($"Student: {this.GetFullName()}'s exam list is empty!");
+            }
+
+            if (this.Exams.Count == 0)
+            {
+                throw new ArgumentException($"Student: {this.GetFullName()} doesn't have any exams!");
+            }
+
+            var examScore = new double[this.Exams.Count];
+            var examResults = CheckExams();
+            for (var i = 0; i < examResults.Count; i++)
+            {
+                examScore[i] =
+                    ((double)examResults[i].Grade - examResults[i].MinGrade) /
+                    (examResults[i].MaxGrade - examResults[i].MinGrade);
+            }
+
+            return examScore.Average();
+        }
+
+        public string GetFullName()
+        {
+            var fullName = $"{this.FirstName} {this.LastName}";
+
+            return fullName;
+        }
+    }
+}
