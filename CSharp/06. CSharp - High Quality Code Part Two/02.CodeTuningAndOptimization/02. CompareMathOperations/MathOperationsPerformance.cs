ffbd2ec89343261784892136036f8ba66136025b
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-09-20 23:48:33 +0300

    Added Optimizations hw solution

---
+++ CSharp/06. CSharp - High Quality Code Part Two/02.CodeTuningAndOptimization/02. CompareMathOperations/MathOperationsPerformance.cs
@@ -0,0 +1,129 @@
+﻿namespace _02.CompareMathOperations
+{
+    using System;
+    using System.Diagnostics;
+
+    internal class MathOperationsPerformance
+    {
+        private static readonly Stopwatch sw = new Stopwatch();
+        private static readonly Random rnd = new Random();
+        private const int Count = 1000;
+
+        internal static void Main()
+        {
+            IntTest();
+            LongTest();
+            FloatTest();
+            DoubleTest();
+            DecimalTest();
+        }
+
+        private static void IntTest()
+        {
+            var result = GetRandomValue();
+            sw.Start();
+
+            for (var i = 0; i < Count; i++)
+            {
+                unchecked
+                {
+                    result += GetRandomValue(); 
+                    result -= GetRandomValue(); 
+                    result++; 
+                    result *= GetRandomValue(); 
+                    result /= GetRandomValue(); 
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Int test passed. Total elapsed: {sw.Elapsed}");
+            sw.Reset();
+        }
+
+        private static void LongTest()
+        {
+            long result = GetRandomValue();
+            sw.Start();
+
+            for (var i = 0; i < Count; i++)
+            {
+                unchecked
+                {
+                    result += GetRandomValue(); 
+                    result -= GetRandomValue(); 
+                    result++; 
+                    result *= GetRandomValue(); 
+                    result /= GetRandomValue(); 
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Long test time elapsed: {sw.Elapsed}");
+            sw.Reset();
+        }
+
+        private static void FloatTest()
+        {
+            float result = GetRandomValue();
+            sw.Start();
+
+            for (var i = 0; i < Count; i++)
+            {              
+                result += GetRandomValue(); 
+                result -= GetRandomValue(); 
+                result++; 
+                result *= GetRandomValue(); 
+                result /= GetRandomValue();                 
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Float tests time elapsed: {sw.Elapsed}");
+            sw.Reset();
+        }
+
+        private static void DoubleTest()
+        {
+            double result = GetRandomValue();
+            sw.Start();
+
+            for (var i = 0; i < Count; i++)
+            {                
+                result += GetRandomValue(); 
+                result -= GetRandomValue(); 
+                result++;      
+                result *= GetRandomValue(); 
+                result /= GetRandomValue();                 
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Double test time elapsed {sw.Elapsed}: ");
+            sw.Reset();
+        }
+
+        private static void DecimalTest()
+        {
+            decimal result = GetRandomValue();
+            sw.Start();
+
+            for (var i = 0; i < Count; i++)
+            {
+                unchecked
+                {
+                    result += GetRandomValue();
+                    result -= GetRandomValue();
+                    result++;               
+                    result /= GetRandomValue();
+                }                
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Decimal test time elapsed: {sw.Elapsed}");
+            sw.Reset();
+        }
+
+        public static int GetRandomValue()
+        {
+            return rnd.Next(1, int.MaxValue);
+        }
+    }
+}
---
+++ CSharp/06. CSharp - High Quality Code Part Two/02.CodeTuningAndOptimization/03. CompareMathFunctions/CompareMathFunctions.cs
@@ -0,0 +1,83 @@
+﻿namespace _03.CompareMathFunctions
+{
+    using System;
+    using System.Diagnostics;
+
+    public class CompareMathFunctions
+    {        
+        private static readonly Stopwatch sw = new Stopwatch();
+        private static readonly Random rnd = new Random();
+
+        private const int Count = 1000000;
+        private const int SquareRootPower = 2;
+
+        static void Main()
+        {
+            FloatTest();
+            DoubleTest();
+            DecimalTest();
+        }
+
+        private static void FloatTest()
+        {
+            sw.Start();
+            var result = 0f;
+
+            for (var i = 0; i < Count; i++)
+            {
+                result = (float)Math.Pow(RandomDouble, SquareRootPower); 
+                result = (float)Math.Log10(RandomDouble); 
+                result = (float)Math.Sin(RandomDouble); 
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Float test time elapsed: {sw.Elapsed}");
+            sw.Reset();
+        }
+
+        private static void DoubleTest()
+        {
+            sw.Start();
+            var result = 0d;
+
+            for (var i = 0; i < Count; i++)
+            {
+                result = Math.Pow(RandomDouble, SquareRootPower); 
+                result = Math.Log10(RandomDouble); 
+                result = Math.Sin(RandomDouble); 
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Double test time elapsed {sw.Elapsed}");
+            sw.Reset();
+        }
+
+        private static void DecimalTest()
+        {
+            sw.Start();
+            var result = 0m;
+
+            for (var i = 0; i < Count; i++)
+            {
+                result = (decimal)Math.Pow(RandomDouble, SquareRootPower); 
+                result = (decimal)Math.Log10(RandomDouble); 
+                result = (decimal)Math.Sin(RandomDouble); 
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Decimal test time elapsed: {sw.Elapsed}");
+            sw.Reset();
+        }
+
+        private static double RandomDouble
+        {
+            get
+            {
+                var randomDouble = rnd.NextDouble() * rnd.Next(1, int.MaxValue);
+
+                return randomDouble;
+            }
+        }
+    }
+    
+}
