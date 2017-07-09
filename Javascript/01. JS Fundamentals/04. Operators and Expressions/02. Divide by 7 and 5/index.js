4a97768de6d5bd5d6f410250bb8ace7187b56361
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2017-07-09 20:39:54 +0300

    refactor JavaScript repo

---
+++ Javascript/01. JS Fundamentals/04. Operators and Expressions/02. Divide by 7 and 5/index.js
@@ -0,0 +1,10 @@
+function solve(args) {
+  var input = Number( +args);
+
+  if (input % 5 === 0 && input % 7 === 0) {
+    console.log('true' + ' ' + input);
+  }
+  else {
+    console.log('false' + ' ' + input);
+  }
+}
