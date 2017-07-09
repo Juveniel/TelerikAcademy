d4dab52905d0d66e3c64561a99ac1bf45a393ffa
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2017-07-09 17:33:01 +0300

    rearrange JS repo

---
+++ Javascript/03. JS Applications/04. ExamPractice/FortuneCookies/utils/authorize-user-compiled.js
@@ -0,0 +1,11 @@
+module.exports = function (app, db) {
+  app.use('/api', function (req, res, next) {
+    var authKey = req.headers['x-auth-key'];
+    req.user = db('users').find({
+      authKey: authKey
+    });
+    next();
+  });
+};
+
+//# sourceMappingURL=authorize-user-compiled.js.map
